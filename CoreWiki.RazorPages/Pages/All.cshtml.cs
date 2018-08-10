d2b39da05ad8c3fab8918ac5dd74db9cdad14d4d
Bobthespiffy <andrew.lerch@gmail.com>
2018-08-10 00:22:45 -0400

    Added All page with pagination

---
+++ CoreWiki.RazorPages/Pages/All.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreWiki.RazorPages.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreWiki.RazorPages.Pages
+{
+    public class AllModel : PageModel
+    {
+        private readonly ApplicationDbContext _Context;
+        //private const int _PageSize = 20;
+        private const int _PageSize = 2;
+
+        public AllModel(ApplicationDbContext context)
+        {
+            this._Context = context;
+        }
+
+        [FromQuery]
+        public int PageNumber { get; set; } = 1;
+
+        public int TotalPages { get; set; }
+
+        public IEnumerable<Article> Articles { get; set; }
+
+
+        public async Task OnGet()
+        {
+            Articles = await _Context.Articles
+                .AsNoTracking()
+                .OrderBy(a => a.Topic)
+                .Skip((PageNumber - 1) * _PageSize)
+                .Take(_PageSize)
+                .ToArrayAsync();
+
+            TotalPages = (int)Math.Ceiling((await _Context.Articles.CountAsync()) / (double)_PageSize);
+        }
+    }
+}
